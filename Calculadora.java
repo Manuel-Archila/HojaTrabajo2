2fb22d046b1b3d2666210a810166ca202b8da65f
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2021-02-05 09:41:50 -0600

    Arreglos al programa

--- Calculadora.java
+++ Calculadora.java
@@ -1,31 +1,48 @@
 public class Calculadora implements CalculadoraGeneral {
+  StackVector<Integer> stick = new StackVector<>();
 
   //Metodo que recibe una String con los datos del realiza una operacion
   public String Calculo(String datos){
-    String[] operacion = datos.split(",");
-    int operando1 = Integer.parseInt(operacion[0]);
-    int operando2 = Integer.parseInt(operacion[2]);
-
+    String[] operacion = datos.split(" ");
     String resultado = "";
-    switch (operacion[1]){
-      case "+":
-        resultado = String.valueOf(operando1 + operando2);
-        break;
+    for(String dato : operacion){
+      int isnum = 0;
+      boolean num = false;
+      try{
+          isnum = Integer.parseInt(dato);
+          num = true;
+      }catch(Exception e){
+         
+      }
+      if(num == true){
+          stick.push(isnum);
+      }else{
+          int num1 = stick.pop();
+          int num2 = stick.pop();
+          switch (dato){
+            case "+":
+              resultado = String.valueOf(num1 + num2);
+              System.out.println(resultado);
+              break;
 
-      case "-":
-        resultado = String.valueOf(operando1 - operando2);
-        break;
-      
-      case "/":
-        resultado = String.valueOf(operando1 * operando2);
-        break;
+            case "-":
+              resultado = String.valueOf(num2 -num1);
+              System.out.println(resultado);
+              break;
+            
+            case "/":
+              resultado = String.valueOf(num2 / num1);
+              System.out.println(resultado);
+              break;
 
-      case "*":
-        resultado = String.valueOf(operando1 / operando2);
-        break;
-
-    }
-    return resultado;
-  }
-  
+            case "*":
+              resultado = String.valueOf(num1 * num2);
+              System.out.println(resultado);
+              break;
+          }
+          stick.push(Integer.parseInt(resultado));
+        }
+}
+return resultado;
+}
 }
--- Main.java
+++ Main.java
@@ -9,37 +9,13 @@ public class Main {
         try {
             Scanner entrada = new Scanner(new File("datos.txt"));
             while (entrada.hasNextLine()) {
-                datos += entrada.nextLine();
-                System.out.println(datos);
+                datos = entrada.nextLine();
+                String resulta = calculator.Calculo(datos);
+                System.out.println("El resultado de la operacion es " + resulta);
             }
             entrada.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        String[] operacion = datos.split(" ");
-        StackVector<Integer> operation = new StackVector<>();
-
-        String calcular = "";
-        for(String dato : operacion){
-            int isnum = 0;
-            boolean num = false;
-            try{
-                isnum = Integer.parseInt(dato);
-                num = true;
-            }catch(Exception e){
-               
-            }
-            if(num == true){
-                operation.push(isnum);
-            }else{
-                int num1 = operation.pop();
-                int num2 = operation.pop();
-                calcular = num1 + "," + dato + "," + num2;
-                String result = calculator.Calculo(calcular);
-                int resulta = Integer.parseInt(result);
-                operation.push(resulta);
-            }
-        }
-        System.out.println(operation.peek());
     }
 }
